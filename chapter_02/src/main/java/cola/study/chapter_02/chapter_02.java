16acd7360511a0f841f5b516b68d9549266c98c8
cola0817 <cola0817jz@gmail.com>
2023-09-18 17:58:07 +0800

    chapter_02 init cmomit

---
+++ chapter_02/src/main/java/cola/study/chapter_02/chapter_02.java
@@ -0,0 +1,14 @@
+package cola.study.chapter_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chapter_02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter02);
+    }
+}
